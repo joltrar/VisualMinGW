ca5dd9570e903cc22346fcb7684fd7cfb51fd227
joltrar <39867158+joltrar@users.noreply.github.com>
2025-08-04 21:31:55 -0400

    Create rsrc.h

---
+++ rsrc.h
@@ -0,0 +1,134 @@
+/********************************************************************
+*	Module:	resource.h. This is part of Visual-MinGW.
+*
+*	License:	Visual-MinGW is covered by GNU General Public License, 
+*			Copyright (C) 2001  Manu B.
+*			See license.htm for more details.
+*
+********************************************************************/
+// Static control ----------------------------
+#define IDC_STATIC				-1
+
+// Bitmaps ----------------------------------
+#define IDB_TOOLBAR				10
+#define IDB_TREEVIEW				11
+
+// Preferences Dialog -----------------------
+#define IDD_PREFERENCES			90
+#define IDC_CC_INCDIR				91
+#define IDC_BROWSE				92
+// Find Dialog -------------------------------
+#define IDD_FIND					100
+#define IDC_FINDWHAT				101
+#define IDC_WHOLEWORD			102
+#define IDC_MATCHCASE			103
+#define IDC_REGEXP				104
+#define IDC_WRAP				105
+#define IDC_UNSLASH				106
+#define IDC_DIRECTIONUP			107
+#define IDC_DIRECTIONDOWN		108
+
+// Replace ----------------------------------
+#define IDD_REPLACE				110
+#define IDC_REPLACEWITH			111
+#define IDC_REPLACE				112
+#define IDC_REPLACEALL			113
+#define IDC_REPLACEINSEL			114
+
+// New module ------------------------------
+#define IDD_NEW_MODULE			200
+#define IDC_HEADER				201
+
+// Options ----------------------------------
+#define IDD_OPTION				210
+#define IDC_OPTION_TABS			211
+#define IDC_HELP_BTN				212
+
+// General tab ---------------------------------
+#define IDD_GENERAL_PANE			220
+#define IDC_STATLIB				221
+#define IDC_DLL					222
+#define IDC_CONSOLE				223
+#define IDC_GUIEXE				224
+#define IDC_DBGSYM				225
+#define IDC_LANGC				226
+#define IDC_LANGCPP				227
+#define IDC_MKF_NAME			228
+#define IDC_MKF_DIR				229
+#define IDC_USER_MKF				230
+#define IDC_TGT_NAME			231
+#define IDC_TGT_DIR				232
+
+// Compiler tab -----------------------------
+#define IDD_COMPILER				240
+#define IDC_CPPFLAGS				241
+#define IDC_WARNING				242
+#define IDC_OPTIMIZ				243
+#define IDC_CFLAGS				244
+#define IDC_INCDIRS				245
+
+// Linker tab --------------------------------
+#define IDD_LINKER				250
+#define IDC_LDSTRIP				251
+#define IDC_LDOPTS				252
+#define IDC_LDLIBS				253
+#define IDC_LIBDIRS				254
+
+// About ------------------------------------
+#define IDD_ABOUT				300
+
+// Menu -------------------------------------
+#define ID_MENU                         	1000
+#define IDM_NEW                         	1001
+#define IDM_OPEN                        	1002
+//#define IDM_CLOSE                       	1003
+#define IDM_NEW_PROJECT			1004
+#define IDM_OPEN_PROJECT			1005
+#define IDM_SAVE_PROJECT			1006
+#define IDM_CLOSE_PROJECT			1007
+#define IDM_SAVE                        	1008
+#define IDM_SAVEAS                      	1009
+#define IDM_SAVEALL				1010
+#define IDM_PREFERENCES			1011
+#define IDM_PAGESETUP			1012
+#define IDM_PRINT                       	1013
+#define IDM_QUIT                        	1014
+
+#define IDM_UNDO                        	1020
+#define IDM_REDO                       	1021
+#define IDM_CUT                         	1022
+#define IDM_COPY                        	1023
+#define IDM_PASTE                       	1024
+#define IDM_SELECTALL			1025
+
+#define IDM_FIND                        	1030
+#define IDM_REPLACE				1031
+
+#define IDM_CASCADE				1040
+#define IDM_TILEHORZ                    	1041
+#define IDM_TILEVERT                    	1042
+#define IDM_ARRANGE				1043
+
+#define IDM_NEW_MODULE			1050
+#define IDM_ADD					1051
+#define IDM_REMOVE_FILE			1052
+#define IDM_REMOVE_MODULE		1053
+#define IDM_OPTION				1054
+#define IDM_ZIP_SRCS				1055
+#define IDM_EXPLORE				1056
+
+#define IDM_BUILD				1060
+#define IDM_REBUILDALL			1061
+#define IDM_RUN_TARGET			1062
+#define IDM_MKCLEAN				1063
+#define IDM_MKF_BUILD			1064
+
+#define IDM_TEST				1070
+
+#define IDM_HELP                        	1080
+#define IDM_ABOUT                       	1081
+
+#define ID_POPMENU                      	1100
+
+#define ID_FIRSTCHILD				2000
+
